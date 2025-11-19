354cd8ad01ef19ae16ba5147e980b6e7186307a5
Jacob Williams <jacobwilliams@users.noreply.github.com>
2025-11-19 16:34:36 -0600

    doc updates

--- docs/conf.py
+++ docs/conf.py
@@ -31,11 +31,9 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.autosummary',
     'sphinx.ext.coverage',
 ]
 
-templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
@@ -145,20 +143,6 @@ napoleon_google_docstring = True
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = True
 napoleon_include_private_with_doc = False
-
-# -- Options for autosummary -------------------------------------------------
-autosummary_generate = True  # Automatically generate stub pages
-autosummary_imported_members = False
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
-napoleon_preprocess_types = False
-napoleon_type_aliases = None
-napoleon_attr_annotations = True
 
 # Intersphinx mapping
 intersphinx_mapping = {
