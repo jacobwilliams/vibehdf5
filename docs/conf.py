b539eeceb2f9b2352ed72ee737f1a554b00b5082
Jacob Williams <jacobwilliams@users.noreply.github.com>
2025-11-19 17:19:07 -0600

    sphinx update to avoid warning

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- HTML output -----------------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
 # -- Autodoc configuration -----------------------------------------------------
 
